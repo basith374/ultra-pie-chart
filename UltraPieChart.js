dca288d37b6459446f2ec41a8c283afec3b43a5f
Basith Kunimal <basith@intellicar.in>
2018-12-01 17:28:40 +0530

    pie chart too large, faster tooltips

--- UltraPieChart.js
+++ UltraPieChart.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom';
 import * as d3 from 'd3';
 import './ultrapiechart.css';
 
-var margin = {top:60, left: 80, right: 80, bottom: 100};
-var width = 360 - margin.left - margin.right;
-var radius = width / 2;
-var innerRadius = 55;
-
-var BackCircle = () => {
+var BackCircle = (props) => {
+    let {innerRadius, radius} = props;
     let arc = d3.arc().innerRadius(innerRadius + 6).outerRadius(radius - 6);
     let pie = d3.pie()([1]);
     return <path fill="#777" d={arc(pie[0])}></path>
@@ -24,20 +20,30 @@ export default class UltraPieChart extends Component {
     }
     constructor() {
         super();
-        this.arc = d3.arc().innerRadius(innerRadius).outerRadius(radius).cornerRadius(3);
-        this.outArc = d3.arc().innerRadius(radius + 5).outerRadius(radius + 10);
-        this.coverArc = d3.arc().innerRadius(innerRadius).outerRadius(radius + 5);
-        this.labelArc = d3.arc().innerRadius(radius * .7).outerRadius(radius * .8);
-        this.outLabelArc = d3.arc().innerRadius(radius + 50).outerRadius(radius + 50);
+        this.arc = d3.arc().cornerRadius(3);
+        this.outArc = d3.arc();
+        this.coverArc = d3.arc();
+        this.labelArc = d3.arc();
+        this.outLabelArc = d3.arc();
         this.arcs = d3.pie().padAngle(.025).value(f => f.value);
     }
     componentDidMount() {
         let el = ReactDOM.findDOMNode(this), pn = el.parentNode;
+        let margin = {
+            top: pn.offsetHeight * 20 / 100,
+            left: pn.offsetWidth * 20 / 100,
+            right: pn.offsetWidth * 20 / 100,
+            bottom: pn.offsetHeight * 30 / 100,
+        }
         let height = pn.offsetHeight - margin.top - margin.bottom,
             width = pn.offsetWidth - margin.left - margin.right,
             radius = width / 2,
-            innerRadius = 50;
-        this.setState({height,width,radius,innerRadius});
+            innerRadius = width / 2 * .5;
+        this.arc.innerRadius(innerRadius).outerRadius(radius);
+        this.outArc.innerRadius(radius + 5).outerRadius(radius + 10);
+        this.coverArc.innerRadius(innerRadius).outerRadius(radius + 5);
+        this.outLabelArc.innerRadius(radius * .7).outerRadius(radius * .8);
+        this.setState({height,width,radius,innerRadius,margin});
     }
     pointArc(d) {
         return d3.pie()
@@ -48,15 +54,6 @@ export default class UltraPieChart extends Component {
     pointColors(domain) {
         return d3.scaleOrdinal(d3.schemeCategory10).domain(domain);
     }
-    lineArc(type, d) {
-        if(type == 1) {
-            return d3.arc().innerRadius(radius + 10).outerRadius(radius + 10).centroid(d);
-        } else if(type == 2) {
-            return d3.arc().innerRadius(radius + 20).outerRadius(radius + 20).centroid(d);
-        } else if(type == 3) {
-            return d3.arc().innerRadius(radius + 20).outerRadius(radius + 20).centroid(d);
-        }
-    }
     clickOut = () => {
         let config = this.props.config;
         if(!config.disableTrack && this.state.focused) {
@@ -64,11 +61,9 @@ export default class UltraPieChart extends Component {
             if(config.selected) config.selected(null);
         }
     }
-    renderSvgContents(color) {
+    renderSvgContents(color, pieData) {
         let config = this.props.config;
-        let data = config.data;
-        let pieData = this.arcs(data);
-        let {height,width} = this.state;
+        let {height,width,radius,innerRadius,margin} = this.state;
         let clickOut = this.clickOut;
         return (
             <svg height={height + margin.top + margin.bottom} width={width + margin.left + margin.right}>
@@ -86,24 +81,55 @@ export default class UltraPieChart extends Component {
                 <text x={margin.left+width/2} y={margin.top+height/2+5} textAnchor="middle">{config.name}</text>
                 <rect onClick={clickOut} width={width+margin.left+margin.right} height={height+margin.top+margin.bottom} fill="transparent"></rect>
                 <g transform={`translate(${(width / 2) + margin.left}, ${(height / 2) + margin.top})`} ref="g">
-                    <BackCircle />
+                    <BackCircle {...{radius,innerRadius}} />
                     {pieData.map((d, i) => {
-                        let onMouseOver = () => this.setState({expanded:d.data.name});
-                        let onMouseOut = () => this.setState({expanded:''});
+                        let onMouseOver = (e) => {
+                            this.setState({expanded:d.data.name});
+                            let el = ReactDOM.findDOMNode(this);
+                            let tar = el.querySelector('.upc-ttx');
+                            tar.style.display = 'block';
+                        }
+                        let onMouseOut = () => {
+                            this.setState({expanded:''});
+                            let el = ReactDOM.findDOMNode(this);
+                            let tar = el.querySelector('.upc-ttx');
+                            tar.style.display = 'none';
+                        };
+                        let onMouseMove = e => {
+                            let el = ReactDOM.findDOMNode(this);
+                            let rect = el.getBoundingClientRect();
+                            let ptop = rect.top + document.body.scrollTop;
+                            let pleft = rect.left + document.body.scrollLeft;
+                            let x = e.clientX - pleft;
+                            let y = e.clientY - ptop;
+                            let tar = el.querySelector('.upc-ttx');
+                            tar.innerText = d.data.name + ' : ' + d.data.value;
+                            tar.style.top = y + 'px';
+                            let w = width + margin.left + margin.right;
+                            let h = height + margin.top + margin.bottom;
+                            if(x > w / 2) {
+                                tar.style.right = (w - (x - 15))  + 'px';
+                                tar.style.left = 'auto';
+                            } else {
+                                tar.style.left = (x + 15) + 'px';
+                                tar.style.right = 'auto';
+                            }
+                        }
                         let onClick = () => {
                             if(!config.disableTrack && this.state.focused != d.data.name) {
                                 if(config.selected) config.selected(d.data);
                                 setTimeout(() => this.setState({focused:d.data.name,focusedC:''}), 50);
                             }
                         }
-                        let hovered = this.state.expanded == d.data.name;
                         let clicked = this.state.focused == d.data.name;
-                        let items = [
-                            <path className={['upc-arc', hovered && 'hov', clicked && 'clk'].filter(f => f).join(' ')} key={`${d.data.name}${i}`} fill={d.data.color || color(d.data.name)} d={this.arc(d)} opacity={.9}></path>,
-                            <path className="upc-arc-cov" {...{onMouseOver,onMouseOut,onClick}} key={`${d.data.name}${i}-cover`} fill="transparent" d={this.coverArc(d)}><title>{`${d.data.name} : ${d.data.value}`}</title></path>,
-                        ];
-                        let content = [<g key={i + 'pie'}>{items}</g>];
-                        return content;
+                        let cls = ['upc-arc', clicked && 'clk'].filter(f => f).join(' ');
+                        return <path key={`${d.data.name}${i}`}
+                            {...{onMouseOver,onMouseOut,onClick,onMouseMove}}
+                            className={cls}
+                            fill={d.data.color || color(d.data.name)}
+                            d={this.arc(d)}
+                            opacity={.9}>
+                            </path>
                     })}
                 </g>
                 {this.state.focused && <rect className="ptcvr" width="20" height={height} x={width + margin.left + margin.right - 30} y={margin.top}></rect>}
@@ -140,11 +166,13 @@ export default class UltraPieChart extends Component {
     }
     render() {
         let data = this.props.config.data;
+        let pieData = this.arcs(data);
         var color = d3.scaleOrdinal(['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#e67e22', '#e74c3c', '#95a5a6', '#34495e', '#f1c40f'])
             .domain(data.map(d => d.name));
+        let {radius} = this.state;
         return (
             <div className="upc-con">
-                {this.renderSvgContents(color)}
+                {radius && this.renderSvgContents(color, pieData)}
                 <div className="upc-ftl">
                     {data.map((d, i) => {
                         let text = d.name + ' : ' + d.value;
@@ -154,6 +182,7 @@ export default class UltraPieChart extends Component {
                         </span>;
                     })}
                 </div>
+                <div className="upc-ttx" style={{display:'none'}}></div>
             </div>
         )
     }
